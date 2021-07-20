093b055b6d9abb26e00df7cd12d8d780fb4d3591
Miguel Paredes <miguelorlandoparedes@gmail.com>
2021-07-20 14:42:40 -0400

    tercer commit

--- libreria.h
+++ libreria.h
@@ -7,7 +7,15 @@ typedef struct _tablero{
     char **tablero;
 }Tablero;
 
-void inicializarTablero(Tablero *tablero, int filas, int columnas){
+Tablero *inicializarTablero(int filas, int columnas){
+    Tablero *tablero = NULL;
+
+    tablero = (Tablero*)malloc(sizeof(Tablero));
+    if(tablero == NULL){
+        fprintf(stderr, "Error al inicializar el tablero: falla en malloc.\n");
+        exit(1);
+    }
+
     tablero->filas = filas;
     tablero->columnas = columnas;
 
@@ -29,27 +37,28 @@ void inicializarTablero(Tablero *tablero, int filas, int columnas){
     for(int i = 0; i < tablero->columnas; ++i)
         for(int j = 0; j < tablero->filas; ++j)
             tablero->tablero[i][j] = ' ';
+            
+    return tablero;
 }
 
 void imprimirTablero(Tablero *tablero){
-    for(int i = 0; i < tablero->columnas + 2; ++i)
-        printf("|%c", i + 65);
-    printf("|");
+    for(int i = 0; i < tablero->columnas; ++i)
+        printf("|%i", i + 1);
+    printf("|\n");
     for(int i = 0; i < tablero->columnas; ++i){
         for(int j = 0; j < tablero->filas; ++j){
-            printf("|%c");
+            printf("|%c", tablero->tablero[i][j]);
         }
         printf("|\n");
     }
 }
 
 int realizarJugada(Tablero *tablero, int columna, char jugador){
-    int fila = tablero->filas;
-    while(tablero->tablero[columna][fila] != ' ' && fila >= 0)
-        fila--;
-    if(tablero->tablero[columna][fila] == ' '){
-        tablero->tablero[columna][fila] = jugador;
-        return 0;
+    for(int i = tablero->filas-1; i >= 0; --i){
+        if(tablero->tablero[i][columna] == ' '){
+            tablero->tablero[i][columna] = jugador;
+            return 0;
+        }
     }
     return 1;
 }
@@ -58,4 +67,12 @@ void reiniciarTablero(Tablero *tablero){
     for(int i = 0; i < tablero->columnas; ++i)
         for(int j = 0; j < tablero->filas; ++j)
             tablero->tablero[i][j] = ' ';
+}
+
+int estaLleno(Tablero *tablero){
+    for(int i = 0; i < tablero->columnas; ++i){
+        if(tablero->tablero[0][i] == ' ')
+            return 0;
+    }
+    return 1;
 }
--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "libreria.h"
+
 
 int main(int argc, char const *argv[]){
-    printf("Hola mundo\n");
+    Tablero *tablero = inicializarTablero(5, 5);
+    char p = 'X', aux = 'O';
+    int columna;
+
+    system("clear");
+    while(!estaLleno(tablero)){
+        imprimirTablero(tablero);
+
+        printf("\nIngrese la columna:\n>>> ");
+        scanf("%i", &columna);
+        printf("\n");
+
+        if(!(1 <= columna < tablero->columnas)){
+            printf("Columna invalida, intente con otra.");
+            continue;
+        }
+
+        if(realizarJugada(tablero, columna-1, p)){
+            printf("Columna llena, intente con otra.");
+            continue;
+        }
+
+        p ^= aux;
+        aux ^= p;
+        p ^= aux;
+    }
+    system("clear");
+
+    imprimirTablero(tablero);
     return 0;
 }
